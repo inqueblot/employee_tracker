e02af3eb5db356b209f93b5b36b7a8990a305a0d
inqueblot <notminebydesign@gmail.com>
2021-02-06 16:03:46 -0600

    put mySQL call into a function

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 const inquirer = require('inquirer');
+const connect = require('./db_connection')
+
+console.log('EMPLOYEE DATABASE')
+
+// connect();
+
+
+inquirer.prompt([
+    {
+        type: 'list',
+        name: 'action',
+        message: 'What would you like to do?',
+        choices: ['View All Employees', 'Add Employee', 'Update Employee Role']
+    }
+])
