96583561704731a9e013002232d8faeccf7c7862
inqueblot <notminebydesign@gmail.com>
2021-02-06 20:52:27 -0600

    list of manager and role when creating an employee

--- index.js
+++ index.js
@@ -2,24 +2,38 @@ const inquirer = require('inquirer');
 const connection = require('./db_connection')
 const ConsoleTab = require('console.table');
 const roleArr = []
-const managerArr = [1]
+const managerArr = []
 const departmentArr = []
 
 
 console.log('EMPLOYEE DATABASE')
-
-connection.query('SELECT id FROM role', function (err, res) {
-    if (err) throw err;
-    res.forEach(element => {
-        roleArr.push(element.id)
+//populates the roleArr
+const popRole = function () {
+    connection.query('SELECT id FROM role', function (err, res) {
+        if (err) throw err;
+        res.forEach(element => {
+            roleArr.push(element.id)
+        });
     });
-});
-connection.query('SELECT id FROM department', function (err, res) {
-    if (err) throw err;
-    res.forEach(element => {
-        departmentArr.push(element.id)
+}
+//populates the departmentArr
+const popDepartment = function () {
+    connection.query('SELECT id FROM department', function (err, res) {
+        if (err) throw err;
+        res.forEach(element => {
+            departmentArr.push(element.id)
+        });
     });
-});
+};
+//populates the managerArr
+const popManager = function () {
+    connection.query('SELECT id FROM employee WHERE role_id = 1', function (err, res) {
+        if (err) throw err;
+        res.forEach(element => {
+            managerArr.push(element.id)
+        });
+    });
+};
 
 const tableDisplay = function () {
     connection.query('SELECT * FROM employee', function (err, res) {
@@ -67,8 +81,16 @@ const addEmployee = function () {
     connection.query('SELECT id, title FROM role', function (err, res) {
         if (err) throw err;
         console.table(res);
-        empQuestions();
+        managerTab();
     });
+
+    const managerTab = function () {
+        connection.query('SELECT id, first_name, last_name FROM employee WHERE role_id = 1', function (err, res) {
+            if (err) throw err;
+            console.table(res)
+            empQuestions();
+        })
+    };
 
     const empQuestions = function () {
         inquirer
@@ -118,44 +140,52 @@ const addEmployee = function () {
 };
 
 const addRole = function () {
-    inquirer
-        .prompt([
-            {
-                type: 'input',
-                name: 'title',
-                message: "What is title of the role?",
-                validate: function (value) {
-                    if (!value)
-                        return "please enter a name"
-                    return true
-                }
+    connection.query('SELECT * FROM department', function (err, res) {
+        if (err) throw err;
+        console.table(res);
+        roleQuestions();
+    });
+    const roleQuestions = function () {
+        inquirer
+            .prompt([
+                {
+                    type: 'input',
+                    name: 'title',
+                    message: "What is title of the role?",
+                    validate: function (value) {
+                        if (!value)
+                            return "please enter a name"
+                        return true
+                    }
 
-            },
-            {
-                type: 'input',
-                name: 'salary',
-                message: "What is the yearly salary for this role?",
-                validate: function (value) {
-                    var pass = parseInt(value);
-                    if (!Number.isInteger(pass))
-                        return "please enter a number"
-                    return true
-                }
-            },
-            {
-                type: 'list',
-                name: 'department_id',
-                message: "What department does this role belong to?",
-                choices: departmentArr,
-            },
-        ]).then((answers) => {
-            console.log(answers)
-            connection.query("INSERT INTO role SET ?", answers, function (err, res) {
-                if (err) throw err;
+                },
+                {
+                    type: 'input',
+                    name: 'salary',
+                    message: "What is the yearly salary for this role?",
+                    validate: function (value) {
+                        var pass = parseInt(value);
+                        if (!Number.isInteger(pass))
+                            return "please enter a number"
+                        return true
+                    }
+                },
+                {
+                    type: 'list',
+                    name: 'department_id',
+                    message: "What department does this role belong to?",
+                    choices: departmentArr,
+                },
+            ]).then((answers) => {
+
+                connection.query("INSERT INTO role SET ?", answers, function (err, res) {
+                    if (err) throw err;
+                });
+                popRole();
+                tableDisplay();
             });
-            tableDisplay();
-        });
-};
+    };
+}
 
 const addDepartment = function () {
     inquirer
@@ -171,12 +201,17 @@ const addDepartment = function () {
                 }
             },
         ]).then((answers) => {
-            console.log(answers)
+
             connection.query("INSERT INTO department SET ?", answers, function (err, res) {
                 if (err) throw err;
             });
+            popDepartment();
             tableDisplay();
         });
 };
 
+
+popDepartment();
+popManager();
+popRole();
 tableDisplay();
